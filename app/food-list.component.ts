6f73c0e8b81cd0f732da6a482b915be93daa0fd4
Rick1970 <rthornbrug@gmail.com>
2016-08-26 14:35:53 -0700

    working add food component created.  All entry fields work.

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -2,17 +2,19 @@ import { Component, EventEmitter } from 'angular2/core';
 import { Food } from './food.model';
 import { FoodComponent} from './food.component';
 import { EditFoodDetailsComponent } from './edit-food-details.component';
+import { NewFoodComponent } from './new-food.component';
 
 
 @Component({
   selector: 'food-list',
   inputs: ['foodList'],
   outputs: ['onFoodSelect'],
-  directives: [FoodComponent, EditFoodDetailsComponent],
+  directives: [FoodComponent, EditFoodDetailsComponent, NewFoodComponent],
   template: `
 
   <food-display *ngFor="#currentFood of foodList" (click)= "foodClicked(currentFood)" [class.selected]="currentFood === selectedFood"[food]="currentFood"></food-display>
   <edit-food-details *ngIf="selectedFood" [food]="selectedFood"></edit-food-details>
+  <new-food (onSubmitNewFood)="createFood($event)"></new-food>
   `
 })
 
@@ -27,7 +29,12 @@ export class FoodListComponent {
   foodClicked(clickedFood: Food): void{
     console.log("child",clickedFood);
     this.selectedFood = clickedFood;
-    this.onFoodSelect.emit(clickedFood);
-  }
+    this.onFoodSelect.emit(clickedFood)
 
+  }
+  createFood(food): void {
+      this.foodList.push(
+        new Food(food.userName, food.userDetails, food.userCalories)
+      )
+    }
 }
---
+++ app/new-food.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter} from 'angular2/core';
+import {Food} from './food.model';
+
+@Component({
+  selector: 'new-food',
+  outputs: ['onSubmitNewFood'],
+  template: `
+  <div class="food-form">
+  <h3>Create Food:</h3>
+  <h4>Enter Name:</h4>
+  <input placeholder="Name" class="input-lg"#newName>
+  <h4>Enter Details:</h4>
+  <input placeholder="Details" class="input-lg"#newDetails>
+  <h4>Enter Calories:</h4>
+  <input placeholder="Calories" class="input-lg"#newCalories>
+
+  <button (click)="addFood(newName, newDetails, newCalories)">Add</button>
+</div>
+  `
+})
+export class NewFoodComponent {
+  public onSubmitNewFood: EventEmitter<Object>;
+  constructor(){
+    this.onSubmitNewFood = new EventEmitter();
+  }
+  addFood(userName: HTMLInputElement, userDetails: HTMLInputElement, userCalories: HTMLInputElement){
+    this.onSubmitNewFood.emit({userName: userName.value, userDetails: userDetails.value, userCalories: userCalories.value});
+    userName.value = "";
+    userDetails.value = "";
+    userCalories.value = "";
+
+  }
+}
