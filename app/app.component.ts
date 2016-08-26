539fc03eeb52520235411a6f62e1d31f6b158119
Rick1970 <rthornbrug@gmail.com>
2016-08-26 12:31:00 -0700

    working foodlist component created.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from 'angular2/core';
 import { Food } from './food.model';
+import {FoodListComponent} from './food-list.component';
 
 @Component({
   selector: 'my-app',
+  directives: [FoodListComponent],
   template: `
     <div class="container">
       <h1>Meal Master Express.</h1>
-      <h3 *ngFor="#food of foods">{{food.name}}</h3>
+      <food-list [foodList]= "foods"></food-list>
     </div>
   `
 })
@@ -19,6 +21,9 @@ export class AppComponent {
   new Food("Pizza", "Slice of pizza at lunch", "380"),
   new Food("Eggs", "Three eggs with breakfast", "300")
 ];
+  }
+  foodWasSelected(clickedFood: Food): void {
+    console.log(clickedFood);
   }
 
 }
--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,2 +1,20 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { Food } from './food.model';
+
+
+@Component({
+  selector: 'food-list',
+  inputs: ['foodList'],
+  template: `
+  <div class="container">
+  <h3 *ngFor="#currentFood of foodList" (click)= "foodClicked(currentFood)">{{currentFood.name}}</h3>
+  </div>
+  `
+})
+
+export class FoodListComponent {
+  public foodList: Food[];
+  foodClicked(clickedFood: Food): void{
+    console.log(clickedFood);
+  }
+}
