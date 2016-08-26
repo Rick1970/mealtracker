8013a7a2088a5d5517ae7d0f6d332b927ba09951
Rick1970 <rthornbrug@gmail.com>
2016-08-26 11:56:11 -0700

    initial work on AppComponent.  Displays backend forced items on the screen.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from 'angular2/core';
+import { Food } from './food.model';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>Meal Master Express.</h1>
+      <h3 *ngFor="#food of foods">{{food.name}}</h3>
     </div>
   `
 })
 export class AppComponent {
+  public foods: Food[];
+  constructor(){
+    this.foods = [
+  new Food("Whopper", "Burger King food.", "860"),
+  new Food("Bacon", "Breakfast.", "300"),
+  new Food("Pizza", "Slice of pizza at lunch", "380"),
+  new Food("Eggs", "Three eggs with breakfast", "300")
+];
+  }
 
 }
